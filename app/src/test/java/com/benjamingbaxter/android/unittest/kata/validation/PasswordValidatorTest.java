1e743ae30206dda0dd4bc75b7b8b2273a73577ef
Baxter, Benjamin <benjamin.baxter@kroger.com>
2015-11-15 20:50:08 -0500

    implement power mock

--- app/src/test/java/com/benjamingbaxter/android/unittest/kata/validation/PasswordValidatorTest.java
+++ app/src/test/java/com/benjamingbaxter/android/unittest/kata/validation/PasswordValidatorTest.java
@@ -1,20 +1,33 @@
 package com.benjamingbaxter.android.unittest.kata.validation;
 
+import android.text.TextUtils;
+import android.widget.TextView;
+
 import com.benjamingbaxter.android.unittest.kata.R;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({TextUtils.class})
 public class PasswordValidatorTest {
 
     FieldValidator validator;
 
     @Before
     public void setup() {
+        mockStatic(TextUtils.class);
+
         validator = new PasswordValidator();
     }
 
@@ -26,12 +39,16 @@ public class PasswordValidatorTest {
 
     @Test
     public void nullPassword() {
+        when(TextUtils.isEmpty(anyString())).thenReturn(true);
+
         int status = validator.validate(null);
         assertThat(status, is(equalTo(R.string.error_field_required)));
     }
 
     @Test
     public void emptyPassword() {
+        when(TextUtils.isEmpty(anyString())).thenReturn(true);
+        
         int status = validator.validate("");
         assertThat(status, is(equalTo(R.string.error_field_required)));
     }
